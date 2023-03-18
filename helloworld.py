a92163ba75b256b19d56586ec952babf213f713e
Chandra Prakash Choubey <43923143+cpc27@users.noreply.github.com>
2023-03-18 13:49:26 +0530

    A (#3)
    * helloworld.py
    
    * Add of while loop
    
    * Addition of lint Git--Gitt
    
    * reason->reasons
    
    ---------
    
    Co-authored-by: cpc27 <cpc.chandra123@gamail.com>

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,10 @@ print("HelloWorld")
 
 for i in range(0, 10):
     print("CPC")
+
+while 1 > 2 :
+    print("Reasons to live")
+else:
+    print("No reason to liveS")
+print("Git ---Gittt")
+    print("CPC")
